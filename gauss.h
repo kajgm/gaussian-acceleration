2db81b2a620d9fe8672f8f40c65c4dfc7da31211
jacobaforrest <91097464+jacobaforrest@users.noreply.github.com>
2023-11-23 17:56:53 -0800

    forgot header

--- gauss.h
+++ gauss.h
@@ -1,10 +1,17 @@
 #ifndef _H_GAUSS_H_
 #define _H_GAUSS_H_
 
-#define SIZE 4096
+#define SIZE 1024
 #define RAND_SEED 123
 
-#define TILE_SIZE 128
+#define TILE_SIZE 64
+#define PACK_COUNT 16
+
+// packed float type: used to help maximize BRAM Utilization by increasing data-width
+struct p16x32f
+{
+    float f[PACK_COUNT];
+};
 
 void gauss(float A[SIZE * SIZE], float B[SIZE], float X[SIZE]);
 
