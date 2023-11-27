b45311bdcd52b4d4083730c7f9970a4538c4a48e
jacobaforrest <91097464+jacobaforrest@users.noreply.github.com>
2023-11-26 19:49:25 -0800

    separated hw_emu and hw

---
+++ FPGA_CODE/Part5_Host_hw/src/gauss.h
@@ -0,0 +1,20 @@
+#ifndef _H_GAUSS_H_
+#define _H_GAUSS_H_
+
+#define SIZE 1024
+#define RAND_SEED 123
+
+#define TILE_SIZE 64
+#define PACK_COUNT 16
+
+// packed float type: used to help maximize BRAM Utilization by increasing data-width
+struct p16x32f
+{
+    float f[PACK_COUNT];
+};
+
+extern "C" {
+    void gauss(p16x32f A[SIZE * SIZE / PACK_COUNT], float B[SIZE], float X[SIZE]);
+}
+
+#endif // _H_GAUSS_H_
