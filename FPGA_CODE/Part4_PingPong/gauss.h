53daca00a7d44d101c951ee430e1d293276e79db
jacobaforrest <91097464+jacobaforrest@users.noreply.github.com>
2023-11-26 19:42:33 -0800

    fixed header

--- FPGA_CODE/Part4_PingPong/gauss.h
+++ FPGA_CODE/Part4_PingPong/gauss.h
@@ -13,6 +13,6 @@ struct p16x32f
     float f[PACK_COUNT];
 };
 
-void gauss(float A[SIZE * SIZE], float B[SIZE], float X[SIZE]);
+void gauss(p16x32f A[SIZE * SIZE / PACK_COUNT], float B[SIZE], float X[SIZE]);
 
 #endif // _H_GAUSS_H_
